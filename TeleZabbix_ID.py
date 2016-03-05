b96f86f3fab854d2db1cdeffe20efee4b18e2ebe
Matheus Bernardes <mthbernardes@gmail.com>
2016-03-05 12:33:47 -0300

    Create TeleZabbix_ID.py

---
+++ TeleZabbix_ID.py
@@ -0,0 +1,26 @@
+import telepot,time
+
+Work = True
+
+def get_conf():
+    from ConfigParser import ConfigParser
+    config = ConfigParser()
+    config.read('TeleZabbix.conf')
+    api = config.get('Telegram','api')
+    return api
+
+def message(msg):
+    global Work
+    print 'Group ID:' , msg['chat']['id']
+    print 'Group Name: ' + msg['chat']['title']
+    print
+    print 'Username ID:', msg['from']['id']
+    print 'Username: ' + msg['from']['username']
+    Work = False
+
+api = get_conf()
+bot = telepot.Bot(api)
+bot.notifyOnMessage(message)
+
+while Work == True:
+    time.sleep(1)
